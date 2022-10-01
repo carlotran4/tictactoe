ba7dcd42cb7d3ecbcab5a0a295eb1fc5724fe82e
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-10-01 17:26:55 -0500

    clearboard ui and implementation

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.tictactoe
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
 
@@ -20,11 +22,19 @@ class MainActivity : AppCompatActivity() {
         val b31 = findViewById<ImageButton>(R.id.button_3_1)
         val b32 = findViewById<ImageButton>(R.id.button_3_2)
         val b33 = findViewById<ImageButton>(R.id.button_3_3)
+        val buttonList: List<ImageButton> = listOf(b11,b12,b13,b21,b22,b23,b31,b32,b33)
+        val clearBoardButton = findViewById<Button>(R.id.clear_board_button)
+        val noImage = R.drawable.default_button_image
         val xImage = R.drawable.x
         val oImage = R.drawable.o
         var firstPlayer = true
 
-        //TODO: EMPTY BOARD BUTTON AND FUNCTION
+        fun clearBoard() {
+            for(b in buttonList){
+                b.setImageResource(noImage)
+                b.contentDescription = "unclicked"
+            }
+        }
 
         fun isEmpty(b:ImageButton):Boolean {
             if (b.contentDescription == "unclicked") return true
@@ -67,6 +77,9 @@ class MainActivity : AppCompatActivity() {
         }
         b33.setOnClickListener{
             setLetter(b33)
+        }
+        clearBoardButton.setOnClickListener{
+            clearBoard()
         }
 
 
