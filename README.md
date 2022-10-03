00b701f5eb6e4dcd7597a3e0714bb11004ef0981
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-10-02 20:33:54 -0500

    implemented winner checking

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A two person tic tac toe game
 What I learned/improved upon-
 - Image handling in android studio
 - GIMP
+- Multidimensional arrays
--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -17,6 +17,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val lines = arrayOf(
+            arrayOf(arrayOf(0,0), arrayOf(0,1), arrayOf(0,2)),
+            arrayOf(arrayOf(1,0), arrayOf(1,1), arrayOf(1,2)),
+            arrayOf(arrayOf(2,0), arrayOf(2,1), arrayOf(2,2)),
+            arrayOf(arrayOf(0,0), arrayOf(1,0), arrayOf(2,0)),
+            arrayOf(arrayOf(0,1), arrayOf(1,1), arrayOf(2,1)),
+            arrayOf(arrayOf(0,2), arrayOf(1,2), arrayOf(2,2)),
+            arrayOf(arrayOf(0,0), arrayOf(1,1), arrayOf(2,2)),
+            arrayOf(arrayOf(2,0), arrayOf(1,1), arrayOf(0,2)),
+        )
+        var board = arrayOf(
+            arrayOf(0,0,0),
+            arrayOf(0,0,0),
+            arrayOf(0,0,0)
+        )
+
+        val coord11 = arrayOf(0,0)
+        val coord12 = arrayOf(0,1)
+        val coord13 = arrayOf(0,2)
+        val coord21 = arrayOf(1,0)
+        val coord22 = arrayOf(1,1)
+        val coord23 = arrayOf(1,2)
+        val coord31 = arrayOf(2,0)
+        val coord32 = arrayOf(2,1)
+        val coord33 = arrayOf(2,2)
+
         val b11 = findViewById<ImageButton>(R.id.button_1_1)
         val b12 = findViewById<ImageButton>(R.id.button_1_2)
         val b13 = findViewById<ImageButton>(R.id.button_1_3)
@@ -45,6 +71,11 @@ class MainActivity : AppCompatActivity() {
                 winnerText.visibility=View.GONE
                 playAgainText.visibility=View.GONE
             }
+            board = arrayOf(
+                arrayOf(0,0,0),
+                arrayOf(0,0,0),
+                arrayOf(0,0,0))
+            firstPlayer=true
         }
 
         fun isEmpty(b:ImageButton):Boolean {
@@ -53,12 +84,14 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        fun setLetter(b: ImageButton){
+        fun setLetter(b: ImageButton,coordinates:Array<Int>){
             if(isEmpty(b)) {
                 if (firstPlayer) b.setImageResource(xImage)
                 else b.setImageResource(oImage)
                 firstPlayer = !firstPlayer
                 b.contentDescription = "clicked"
+                if(!firstPlayer)board[coordinates[0]][coordinates[1]]=1
+                else board[coordinates[0]][coordinates[1]]=2
             }
         }
 
@@ -71,49 +104,72 @@ class MainActivity : AppCompatActivity() {
             playAgainText.visibility=View.VISIBLE
         }
 
-        fun checkForWinner(){
-            val winner = ""
-
-            if(winner != ""){
-                displayWinner(winner)
+        fun checkLine(line:Array<Array<Int>>):Int{
+            var player1 = 0
+            var player2 = 0
+            for(point in line){
+                if(board[point[0]][point[1]]==1){
+                    player1++
+                }
+                if(board[point[0]][point[1]]==2){
+                    player2++
+                }
             }
+            if(player1==3){
+                return 1
+            }
+            if(player2==3){
+                return 2
+            }
+            return 0
+        }
+
+        fun checkForWinner(){
+            var winner = 0
+            for(line in lines){
+                if(checkLine(line)==1) winner = 1
+                if(checkLine(line)==2) winner = 2
+            }
+            if(winner==1) displayWinner("x")
+            if(winner==2) displayWinner("o")
+
         }
 
 
         b11.setOnClickListener {
-            setLetter(b11)
+            setLetter(b11,coord11)
             checkForWinner()
         }
         b12.setOnClickListener {
-            setLetter(b12)
+            setLetter(b12,coord12)
             checkForWinner()
         }
         b13.setOnClickListener {
-            setLetter(b13)
+            setLetter(b13,coord13)
             checkForWinner()
         }
         b21.setOnClickListener {
-            setLetter(b21)
+            setLetter(b21,coord21)
             checkForWinner()
         }
         b22.setOnClickListener{
-            setLetter(b22)
+            setLetter(b22,coord22)
             checkForWinner()
         }
         b23.setOnClickListener{
-            setLetter(b23)
+            setLetter(b23,coord23)
             checkForWinner()
         }
         b31.setOnClickListener{
-            setLetter(b31)
+            setLetter(b31,coord31)
             checkForWinner()
         }
         b32.setOnClickListener{
-            setLetter(b32)
+            setLetter(b32,coord32)
             checkForWinner()
         }
         b33.setOnClickListener{
-            setLetter(b33)
+            setLetter(b33,coord33)
             checkForWinner()
         }
         clearBoardButton.setOnClickListener{
